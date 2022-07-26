af12de9d6c72eb2b7b28a6b5da9c236edbdaadfc
Wagner <wagnerdmll1994@gmail.com>
2022-07-26 19:17:02 -0300

    React DOM replaced by Create Root (index.js)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 
-import App from './App';
-import './App.scss';
+import App from "./App";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+root.render(<App />);
