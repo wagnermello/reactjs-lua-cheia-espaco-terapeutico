c358e28a91debf9727884630507bff206b2a027f
Wagner <wagnerdmll1994@gmail.com>
2022-07-27 11:48:57 -0300

    Website finished

--- src/components/Copyright/Copyright.jsx
+++ src/components/Copyright/Copyright.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./Copyright.scss";
 
 function Copyright() {
 	return (
 		<div className="copyright__container">
 			<p>Copyright 2022 - Instituto Espiritual Xamânico Lua Cheia</p>
-			<a href="/politica-de-privacidade">Política e Termos de Privacidade</a>
+			<Link to="/politica-de-privacidade" className="copyright__link">
+				Política de Privacidade
+			</Link>
 		</div>
 	);
 }
--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import AtendimentosPage from "./pages/AtendimentosPage/AtendimentosPage";
 import LocalizacaoPage from "./pages/LocalizacaoPage/LocalizacaoPage";
 import MedicinasFlorestaPage from "./pages/MedicinasFlorestaPage/MedicinasFlorestaPage";
-import PoliticasPage from "./pages/PoliticasPage/PoliticasPage";
+import PoliticaPage from "./pages/PoliticaPage/PoliticaPage";
 import Copyright from "./components/Copyright/Copyright";
 import ScrollToTop from "./components/Navbar/ScrollToTop";
 
@@ -21,7 +21,7 @@ function RoutesApp() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="/atendimentos" element={<AtendimentosPage />} />
 				<Route path="/localizacao" element={<LocalizacaoPage />} />
-				<Route path="/politica-de-privacidade" element={<PoliticasPage />} />
+				<Route path="/politica-de-privacidade" element={<PoliticaPage />} />
 				<Route
 					path="/medicinas-da-floresta"
 					element={<MedicinasFlorestaPage />}
